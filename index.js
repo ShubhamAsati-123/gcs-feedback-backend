54ebe6bc5e69ac3093bd70320bfe3ad7671aeeaf
JokerSA <74976564+ShubhamAsati-123@users.noreply.github.com>
2024-06-09 18:18:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors()); // Enable CORS for all routes
+app.use(bodyParser.json());
+
+let db;
+
+// MongoDB connection
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client
+  .connect()
+  .then(() => {
+    db = client.db();
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("GCS Feedback Backend");
+});
+
+app.post("/submit", (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Save to MongoDB
+  const inquiriesCollection = db.collection("feedback");
+  inquiriesCollection
+    .insertOne({ name, email, message })
+    .then((result) => {
+      res
+        .status(200)
+        .send(`Inquiry feedback from ${name} (${email}): ${message}`);
+    })
+    .catch((err) => {
+      console.error("Failed to save feedback", err);
+      res.status(500).send("Failed to save feedback");
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
